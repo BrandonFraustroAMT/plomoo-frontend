912f842864a652fc29694877ea3c5dcb7311b879
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-06-24 20:37:59 -0600

    Adding favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,4 +25,15 @@ export default defineNuxtConfig({
      },
    ],
   },
+  head: {
+    title: 'Plomo Media',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Plomo media' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: './public/favicon.ico' }
+    ]
+  },
 })
